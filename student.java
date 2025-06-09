0c15bb4ef04706de003d3ed8a5cc4edfd8f74249
lahari-1838 <laharimurahari@gmail.com>
2025-06-09 11:40:16 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,15 @@
+package ABC;
+import java.util.HashMap;
+
+public class student {
+	public static void main(String[] args) {
+		HashMap<Integer,String>student=new HashMap<Integer,String>();
+		student.put(101, "Ravi");
+		student.put(102, "Jaya");
+		student.put(103, "jara");
+		student.put(104, "sara");
+		student.put(105, "tara");
+		System.out.println(student);
+		
+	}
+}
