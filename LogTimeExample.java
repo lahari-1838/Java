13b4dc8d0eb36e2c9a93da4fb5e86184e0e4fc40
lahari-1838 <laharimurahari@gmail.com>
2025-06-11 21:19:00 +0530

    Add files via upload

---
+++ LogTimeExample.java
@@ -0,0 +1,27 @@
+package ABC;
+
+public class LogTimeExample {
+
+	public static void main(String[] args) {
+		int[] numbers= {1,2,3,5,7,9,11,13};
+		int target=7;
+		int low=0;
+		int high=numbers.length-1;
+		while(low<=high) {
+			int mid=(low+high)/2;
+			if(numbers[mid]==target) {
+				System.out.println("Food number"+target+"at position"+mid);
+				return;
+				
+			}else if (numbers[mid]<target) {
+				low=mid+1;
+			}else {
+				high=mid-1;
+			}
+		}
+		System.out.println("Number not found.");
+		// TODO Auto-generated method stub
+
+	}
+
+}
