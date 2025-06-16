634ad3c94048702130c317e173adb99379ccbb74
lahari-1838 <laharimurahari@gmail.com>
2025-06-16 22:31:37 +0530

    Add files via upload

---
+++ simplewrapper.java
@@ -0,0 +1,20 @@
+package ABC;
+
+public class simplewrapper {
+	
+	
+
+		public static void main(String[] args) {
+			int a=1;
+			Integer b=1;
+			double price=99.99;
+			Double price2=99.99;
+			System.out.println(a);
+			System.out.println(b);
+
+		}
+
+	}
+
+
+
